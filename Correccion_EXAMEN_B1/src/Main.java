3cc30e64f35524cd215d5b04e5a87f8ba416cbc9
1-21-01-E043-29\Esf0t <kevin.mendoza01@epn.edu.ec>
2024-06-20 08:52:51 -0500

    TODO

--- Correccion_EXAMEN_B1/src/Main.java
+++ Correccion_EXAMEN_B1/src/Main.java
@@ -6,56 +6,73 @@ import Rutas.Pasajero;
 public class Main {
     public static void main(String[] args) {
 
+        // Integrantes: WALTER COBACANGO, RICHARD PADILLA, KEVIN MENDOZA
+
         // AQUI ESTAN VALORES QUEMAODS CON CLINETES Y RUTAS
-        Pasajero cliente1 = new Pasajero(" Walter", "Cobacango", "Ponceano", "0979376396", 18);
-        Pasajero cliente2 = new Pasajero("Kevin", "Mendoza", "Vicentina", "0984168469", 34);
-        Pasajero cliente3 = new Pasajero("Richard", "Acero", "Zabala", "0999999956", 56);
-        Pasajero cliente4 = new Pasajero("Martha", "Cobacango", "Cancelen", "09867543929", 30);
-        Pasajero cliente5 = new Pasajero("Azucena", "Acero", "Cayambe", "0995122669", 53);
+        Pasajero cliente1 = new Pasajero(" Walter", "Cobacango", "Ponceano", "0979376396", 18, "Normal");
+        Pasajero cliente2 = new Pasajero("Kevin", "Mendoza", "Vicentina", "0984168469", 34, "VIP");
+        Pasajero cliente3 = new Pasajero("Richard", "Acero", "Zabala", "0999999956", 56, "VIP");
+        Pasajero cliente4 = new Pasajero("Martha", "Cobacango", "Cancelen", "09867543929", 30, "Normal");
+        Pasajero cliente5 = new Pasajero("Azucena", "Acero", "Cayambe", "0995122669", 53,"VIP");
 
         Ruta ruta1 = new Ruta("Quito_Guayaquil", 20);
         Ruta ruta2 = new Ruta("Quito-Tulcán", 17.50);
         Ruta ruta3 = new Ruta("Quito-Puyo", 15);
         Ruta ruta4 = new Ruta("Quito-Riobamba", 17.50);
 
+        Normal normal1 = new Normal("Quito-Guayaquil", 20, "Ventana", 2, 1);
+        VIP vip1 = new VIP("Quito-Galápagos", 100, "Primera fila", 3, 2);
 
-        // Valores nulos de rutas y pasajeros
+        // Valor nulos
+        Ruta ruta21 =  new Ruta();
+        Pasajero cliente21 = new Pasajero();
+        Pasajero cliente22 = new Pasajero();
+        Pasajero cliente23 = new Pasajero();
+        Pasajero cliente24 = new Pasajero();
+        Pasajero cliente25 = new Pasajero();
 
-        Pasajero cliente6 = new Pasajero();
-        Ruta ruta5 = new Ruta();
-        Ruta ruta6 = new Ruta();
-        Ruta ruta7 = new Ruta();
-        Ruta ruta8 = new Ruta();
-        Ruta ruta9 = new Ruta();
+        // valores settiados
+        cliente21.setNombre("Walter");
+        cliente21.setApellido("Cobacango");
+        cliente21.setDireccion("Ponceano");
+        cliente21.setTelefono("0979376396");
+        cliente21.setEdad(18);
+        cliente21.setTipoPasaje("Normal");
+        System.out.println("Información del cliente 21 SETTEADO VALORES:");
+        cliente21.mostrarInfoCliente(20);
 
 
-        // Setteo de valores
-        cliente6.setNombre("Carmen");
-        cliente6.setApellido("Lopez");
-        cliente6.setDireccion("Cotocollao");
-        cliente6.setTelefono("0987543207");
-        cliente6.setEdad(29);
-
-        cliente6.mostrarInfoCliente();
 
 
-        // imprimir valores
+        // Calcular y mostrar costo total para cada cliente
+        double costoCliente1 = normal1.total();
+        double costoCliente2 = vip1.total();
+        double costoCliente3 = normal1.total();
+        double costoCliente4 = vip1.total();
+        double costoCliente5 = normal1.total();
 
-        System.out.println("VAlores nulos y setteados");
-        ruta1.mostrar_datos();
+        // Mostrar información de cada cliente con su tipo de pasaje y costo total
         System.out.println();
-        cliente6.mostrarInfoCliente();
+        System.out.println("INFORMACION DE CADA CLIENTE");
+        System.out.println();
+        System.out.println("Información del cliente 1:");
+        cliente1.mostrarInfoCliente(costoCliente1);
         System.out.println();
 
-        System.out.println("Valores quemados");
-        ruta2.mostrar_datos();
+        System.out.println("Información del cliente 2:");
+        cliente2.mostrarInfoCliente(costoCliente2);
         System.out.println();
-        ruta3.mostrar_datos();
+
+        System.out.println("Información del cliente 3:");
+        cliente3.mostrarInfoCliente(costoCliente3);
         System.out.println();
-        ruta4.mostrar_datos();
+
+        System.out.println("Información del cliente 4:");
+        cliente4.mostrarInfoCliente(costoCliente4);
         System.out.println();
-        ruta5.mostrar_datos();
+
+        System.out.println("Información del cliente 5:");
+        cliente5.mostrarInfoCliente(costoCliente5);
         System.out.println();
-        cliente1.mostrarInfoCliente();
     }
 }
--- Correccion_EXAMEN_B1/src/Rutas/Pasajero.java
+++ Correccion_EXAMEN_B1/src/Rutas/Pasajero.java
@@ -6,63 +6,78 @@ public class Pasajero {
     String direccion;
     String telefono;
     int edad;
+    String tipoPasaje;
 
     public Pasajero() {
+
     }
 
-    public Pasajero(String nombre, String apellido, String direccion, String telefono, int edad) {
+    public Pasajero(String nombre, String apellido, String direccion, String telefono, int edad, String tipoPasaje) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.direccion = direccion;
         this.telefono = telefono;
         this.edad = edad;
+        this.tipoPasaje = tipoPasaje;
     }
+
 
     public String getNombre() {
         return nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public int getEdad() {
-        return edad;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    public String getApellido() {
+        return apellido;
+    }
+
     public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
     }
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
+    public String getTelefono() {
+        return telefono;
+    }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
     }
 
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
-    public void mostrarInfoCliente() {
+    public String getTipoPasaje() {
+        return tipoPasaje;
+    }
+
+    public void setTipoPasaje(String tipoPasaje) {
+        this.tipoPasaje = tipoPasaje;
+    }
+
+    // Método para mostrar la información del cliente con el tipo de pasaje y el costo total
+    public void mostrarInfoCliente(double costoTotal) {
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellido: " + apellido);
-        System.out.println("Direccion: " + direccion);
-        System.out.println("Telefono: " + telefono);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
         System.out.println("Edad: " + edad);
+        System.out.println("Tipo de pasaje: " + tipoPasaje);
+        System.out.println("Costo total: " + costoTotal);
     }
 }
